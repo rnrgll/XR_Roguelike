6fb497d37fa0538a69a5d0f125a71b18812e9c5f
rnrgll <shuajjjjj@naver.com>
2025-06-19 17:48:09 +0900

    [Test] 인코딩 확인용 테스트 스크립트 추가

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    //인코딩 확인용 테스트 스크립트입니다.
+    //주석부분의 한글이 깨지지 않고 보이는지 확인해주시면 됩니다.
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
