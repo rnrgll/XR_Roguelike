e402cec56bd64441a47170496ad484fd4e5310af
TheNewBoro <potar1132@gmail.com>
2025-06-23 17:25:45 +0900

    [Feat]TODO-List set
    타이틀 씬 구성을 위한 TODO List 작성

---
+++ Assets/KMH/Script/Shop.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        아우터 상점과 인게임 상점이 다름
+    }
+}
