0ee2478728e932d2ef373e2076ea05506324fa84
seongjunC <163829433+seongjunC@users.noreply.github.com>
2025-06-24 07:06:47 +0900

    [Feat] CardController 제작
    카드에 대한 부분을 담당해주는 카드 컨드롤러를 제작하였고, 추가로 해당 기능에 필요한 RandomManager를 만들어 Manager에 등록해두었다. 추가로 마이너 아르카나의 순서 부분을 cont로 옮기고 메이저 아르카나의 방향 부분을 만들었다.

--- Assets/CSJ/Card/MinorArcana.cs
+++ Assets/CSJ/Card/MinorArcana.cs
@@ -7,16 +7,12 @@ public readonly struct MinorArcana : ICard
 {
     public MinorSuit CardSuit { get; }
     public int CardNum { get; }
-    // order는 카드가 여러 장일때 카드를 구분하도록 순서를 나타냄
-    // 현재는 사용하지 않지만 추후 확장을 대비해 넣음
-    public int CardOrder { get; }
 
 
-    public MinorArcana(MinorSuit _suit, int _num, int _order = 1)
+    public MinorArcana(MinorSuit _suit, int _num)
     {
         CardSuit = _suit;
         CardNum = _num;
-        CardOrder = _order;
     }
 
 }
