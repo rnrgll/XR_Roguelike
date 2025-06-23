86736cd8e5afdf952809755e2cbb739f49c572d7
jihyeog144 <jiheog144@gmail.com>
2025-06-23 17:31:42 +0900

    [Feat] EnemyController
    적 행동 추가

---
+++ Assets/CJH/CJH_Scripts/EnemyController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour, IEnemyActor
+{
+    private void Start()
+    {
+        Managers.Manager.Turn.RegisterEnemy(this);
+    }
+
+    public void TakeTurn()
+    {
+        Debug.Log($"{gameObject.name}의 적 행동 발동!");
+        // 적 행동 로직
+    }
+}
