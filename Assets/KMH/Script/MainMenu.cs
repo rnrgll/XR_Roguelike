f54305b79eb06a4a11f5e354934d3e220abf7d0a
TheNewBoro <potar1132@gmail.com>
2025-06-24 17:45:14 +0900

    [Feat]Option Scene realization
    씬 전환 시 옵션 씬 구현(기능X)

--- Assets/KMH/Script/MainMenu.cs
+++ Assets/KMH/Script/MainMenu.cs
@@ -4,30 +4,28 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnClickStart()
     {
-        
+        Debug.Log("게임시작");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickShop()
     {
-        디자인 빼고 작업
-
-        타이틀씬 및 기능 구현
-        각 버튼마다 각 당담 씬 전환 기능 구현
-
-        게임 시작 씬 구현
-
-        상점씬 구현
-            플레이어가 가지고 있는 재화를 디스플레이로 표시
-            아이템 아이콘과 가격 표시 + 
-
-        옵션 구현
-            음량, 그래픽(해상도 등), 게임플레이 옵션 창구현
-
-        게임 종료 구현
-
+        Debug.Log("상점");
     }
+
+    public void OnClickOption()
+    {
+        Debug.Log("설정");
+    }
+
+    public void OnClickQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+   
 }
