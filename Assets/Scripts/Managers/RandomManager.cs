a238009109d22ab6a1a18801e7822f01f22156ed
seongjunC <163829433+seongjunC@users.noreply.github.com>
2025-06-26 10:10:18 +0900

    [feat] RandInt 메서드 추가 구현
    randomManager에 count만큼의 숫자를 반환하는 메서드를 추가 구현하였다.

--- Assets/Scripts/Managers/RandomManager.cs
+++ Assets/Scripts/Managers/RandomManager.cs
@@ -34,6 +34,34 @@ public class RandomManager : Singleton<RandomManager>
     }
 
     /// <summary>
+    /// 랜덤하게 정수를 반환해준다. count가 max-min 보다 작다면 error를 발생시킨다.
+    /// </summary>
+    /// <param name="min"> 최솟값, 해당 값 이상의 값이 반환된다</param>
+    /// <param name="max"> 최댓값, 해당 값 미만의 값까지 반환된다.</param>
+    /// <param name="count">반환할 개수를 정해준다.</param>
+    /// <return> min <= x < max의 값이 리턴된다. </return>
+    public List<int> RandInt(int min, int max, int count)
+    {
+        int total = max - min + 1;
+        if (count > total)
+            throw new ArgumentException($"반환 할 개수({count})가 반환 값의 범위({total})보다 큽니다.");
+
+        var pool = new List<int>(total);
+        for (int i = min; i < max; i++)
+            pool.Add(i);
+
+        for (int i = pool.Count - 1; i > 0; i--)
+        {
+            int j = Random.Range(0, i + 1);
+            int tmp = pool[i];
+            pool[i] = pool[j];
+            pool[j] = tmp;
+        }
+
+        return pool.GetRange(0, count);
+    }
+
+    /// <summary>
     /// min 이상, max 이하 범위의 랜덤한 float을 반환
     /// </summary>
     /// <param name="min"></param>
