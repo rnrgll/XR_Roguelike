b26aa0e712a36e0a42b1d433fdf3b7d371ffaa27
seongjunC <163829433+seongjunC@users.noreply.github.com>
2025-07-03 11:58:38 +0900

    Update RustSO.cs

--- Assets/CSJ/Card/SO/Debuff/RustSO.cs
+++ Assets/CSJ/Card/SO/Debuff/RustSO.cs
@@ -10,23 +10,10 @@ public class RustSO : CardDebuffSO
     [SerializeField] private int penalty = 50;
     private Action<MinorArcana> playHandler;
 
-    public override void OnSubscribe(MinorArcana card, CardController controller)
+    public override void OnCardPlayed(MinorArcana card, CardController controller)
     {
-        playHandler = c =>
-        {
-            if (c == card)
-            {
-                Debug.Log($"[부식] {card.CardName} 사용 → 데미지 {penalty}만큼 감소");
-                controller.AddPanelty(penalty);
-                // 부식 효과는 사라지지 않고 계속 유지된다
-            }
-        };
-        controller.OnCardSubmited += playHandler;
-    }
-
-    public override void OnUnSubscribe(MinorArcana card, CardController controller)
-    {
-        if (playHandler != null)
-            controller.OnCardSubmited -= playHandler;
+            Debug.Log($"[부식] {card.CardName} 사용 → 데미지 {penalty}만큼 감소");
+            controller.AddPanelty(penalty);
+            // 부식 효과는 사라지지 않고 계속 유지된다
     }
 }
