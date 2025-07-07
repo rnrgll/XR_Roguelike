92fbd750fa6a228bfacc75b94462ce482da7d6ea
seongjunC <163829433+seongjunC@users.noreply.github.com>
2025-07-07 17:28:52 +0900

    [fix] handUI 버전 되돌리기
    현재 빌드를 위해 오류가 있는 핸드 버전을 뒤로 되돌림

--- Assets/CSJ/Card/CardUI/HandUIController.cs
+++ Assets/CSJ/Card/CardUI/HandUIController.cs
@@ -6,14 +6,12 @@ using System.Drawing;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
-
 public class HandUIController : UIRequire
 {
     [SerializeField] private RectTransform handContainer;
     [SerializeField] private GameObject cardPrefab;
     private List<GameObject> spawnedCards = new List<GameObject>();
-    public Action OnCardSetted;
-    public Action<MinorArcana> RefreshAction;
+    public event Action OnCardSetted;
 
     public override void InitializeUI(PlayerController pc)
     {
@@ -23,19 +21,14 @@ public class HandUIController : UIRequire
 
     protected override void Subscribe()
     {
-        RefreshAction = _ => RefreshHand();
         cardController.OnChangedHands += RefreshHand;
         cardController.OnSelectionChanged += SyncUI;
-        cardController.OnCardSelected += RefreshAction;
-        cardController.OnCardDeSelected += RefreshAction;
     }
 
     protected override void UnSubscribe()
     {
         cardController.OnChangedHands -= RefreshHand;
         cardController.OnSelectionChanged -= SyncUI;
-        cardController.OnCardSelected -= RefreshAction;
-        cardController.OnCardDeSelected -= RefreshAction;
     }
 
     public void RefreshHand()
@@ -51,19 +44,24 @@ public class HandUIController : UIRequire
         var hand = cardController.GetHand();
         foreach (var card in hand)
         {
-            if (cardController.SelectedCard.Contains(card)) continue;
             var go = Instantiate(cardPrefab, handContainer);
             var ui = go.GetComponent<CardUI>();
             ui.Setup(card);
 
-
             ui.OnClick += c =>
             {
+                if (cardController.IsUsableDic.ContainsKey(c) &&
+                cardController.IsUsableDic[c] == false) return;
 
-                if (cardController.IsUsableDic.TryGetValue(c, out var usable) &&
-                !usable) return;
-
-                if (cardController.SelectedCard.Count >= 5) return;
+                if (cardController.SelectedCard.Count >= 5)
+                {
+                    if (ui._isSelected)
+                    {
+                        ui.ToggleSelect();
+                        CardDeSelected(c);
+                    }
+                    return;
+                }
 
                 bool now = ui.ToggleSelect();
                 if (now) CardSelected(c);
@@ -78,6 +76,7 @@ public class HandUIController : UIRequire
     {
         cardController.OnCardSelected?.Invoke(card);
     }
+
     private void CardDeSelected(MinorArcana card)
     {
         cardController.OnCardDeSelected?.Invoke(card);
@@ -93,3 +92,90 @@ public class HandUIController : UIRequire
         }
     }
 }
+
+// public class HandUIController : UIRequire
+// {
+//     [SerializeField] private RectTransform handContainer;
+//     [SerializeField] private GameObject cardPrefab;
+//     private List<GameObject> spawnedCards = new List<GameObject>();
+//     public Action OnCardSetted;
+//     public Action<MinorArcana> RefreshAction;
+// 
+//     public override void InitializeUI(PlayerController pc)
+//     {
+//         base.InitializeUI(pc);
+//         RefreshHand();
+//     }
+// 
+//     protected override void Subscribe()
+//     {
+//         RefreshAction = _ => RefreshHand();
+//         cardController.OnChangedHands += RefreshHand;
+//         cardController.OnSelectionChanged += SyncUI;
+//         cardController.OnCardSelected += RefreshAction;
+//         cardController.OnCardDeSelected += RefreshAction;
+//     }
+// 
+//     protected override void UnSubscribe()
+//     {
+//         cardController.OnChangedHands -= RefreshHand;
+//         cardController.OnSelectionChanged -= SyncUI;
+//         cardController.OnCardSelected -= RefreshAction;
+//         cardController.OnCardDeSelected -= RefreshAction;
+//     }
+// 
+//     public void RefreshHand()
+//     {
+//         if (cardController == null) return;
+//         foreach (var go in spawnedCards)
+//         {
+//             Destroy(go);
+//         }
+//         spawnedCards.Clear();
+// 
+//         cardController.SortByStand();
+//         var hand = cardController.GetHand();
+//         foreach (var card in hand)
+//         {
+//             if (cardController.SelectedCard.Contains(card)) continue;
+//             var go = Instantiate(cardPrefab, handContainer);
+//             var ui = go.GetComponent<CardUI>();
+//             ui.Setup(card);
+// 
+// 
+//             ui.OnClick += c =>
+//             {
+// 
+//                 if (cardController.IsUsableDic.TryGetValue(c, out var usable) &&
+//                 !usable) return;
+// 
+//                 if (cardController.SelectedCard.Count >= 5) return;
+// 
+//                 bool now = ui.ToggleSelect();
+//                 if (now) CardSelected(c);
+//                 else CardDeSelected(c);
+//             };
+//             spawnedCards.Add(go);
+//         }
+//         OnCardSetted?.Invoke();
+//     }
+// 
+//     private void CardSelected(MinorArcana card)
+//     {
+//         cardController.OnCardSelected?.Invoke(card);
+//     }
+//     private void CardDeSelected(MinorArcana card)
+//     {
+//         cardController.OnCardDeSelected?.Invoke(card);
+//     }
+// 
+//     private void SyncUI(CardCombinationEnum _)
+//     {
+//         foreach (var go in spawnedCards)
+//         {
+//             var ui = go.GetComponent<CardUI>();
+//             bool shouldBeSelected = cardController.SelectedCard.Contains(ui.CardData);
+//             ui.SyncSelected(shouldBeSelected);
+//         }
+//     }
+// }
