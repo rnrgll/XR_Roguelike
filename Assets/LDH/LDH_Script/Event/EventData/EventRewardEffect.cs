cf490f843ac58da5b14a9cd62d12cc0611e198a3
rnrgll <shuajjjjj@naver.com>
2025-07-07 10:41:47 +0900

    [Fix] 이벤트 효과적용과 다음 씬으로 넘어가는 순서 수정중

--- Assets/LDH/LDH_Script/Event/EventData/EventRewardEffect.cs
+++ Assets/LDH/LDH_Script/Event/EventData/EventRewardEffect.cs
@@ -1,4 +1,5 @@
 using Managers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -11,8 +12,15 @@ namespace Event
         public int? PenaltyCost;
         public int? SubstituteCost;
 
-        public void ApplyEffects()
+        public void ApplyEffects(Action onComplete)
         {
+            int subEffectCount = SubEffectList.Count;
+            if (subEffectCount == 0)
+            {
+                onComplete?.Invoke();
+                return;
+            }
+            
             foreach (SubEffect subEffect in SubEffectList)
             {
                 //서브 이팩트 효과 적용
@@ -21,12 +29,16 @@ namespace Event
                     int gap = Manager.GameState.Gold - subEffect.Value;
                     if (gap < 0)
                     {
-                        //todo : 테스트 필요 
                         var player = TurnManager.Instance.GetPlayerController();
                         player.ChangeMaxHp(-SubstituteCost??0 * gap);
                     }
                 }
-                subEffect.ApplyEffect();
+                subEffect.ApplyEffect(() =>
+                {
+                    subEffectCount--;
+                    if(subEffectCount==0)
+                        onComplete?.Invoke();
+                });
             }
         }
     }
